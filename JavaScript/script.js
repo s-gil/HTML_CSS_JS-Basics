4bd9adef4994b4cb020f5d7d6b6c771c80fb8463
Sebastian_Gil <s.gil@utp.edu.co>
2022-04-17 11:28:01 -0500

    Create new folder for javaScript and Denarios_Restaurant

---
+++ JavaScript/script.js
@@ -0,0 +1,15 @@
+// function OrderChickenWith(SideDish='default'){
+// console.log("chicken with "+ SideDish)
+// }
+
+// OrderChickenWith("rice")
+
+// if(console.log('Hello')){
+//     console.log('HEllo')
+// }
+
+// if('HEllo'){
+//     bool(console.log('YEd'))
+// }
+Boolean(5)
+
